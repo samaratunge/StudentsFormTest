8a8dc021819e21ed65168e29bc40870a34d094d4
Udara Samaratunge <samaratunge@gmail.com>
2025-02-24 11:55:19 +1300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace FormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Display Udara Text";
+        }
+    }
+}
